8ddbdfa67e0c259d3a788f6f9c0b23cb39761643
heyaoyu <heyaoyu@youku.com>
2014-05-07 16:53:36 +0800

    Javascript jsonp problem finding and new panel2.html. panel.html records the werid problem.

--- Javascript/js/jsonp.js
+++ Javascript/js/jsonp.js
@@ -1,7 +1,7 @@
 function jsonp(){
     var script = document.createElement("script");
     script.src = "http://localhost:8888/jsonp_service";
-    document.getElementsByTagName('head')[0].appendChild(script);  
+    document.getElementsByTagName('head')[0].appendChild(script);
 }
 
 function callback(server_str){
