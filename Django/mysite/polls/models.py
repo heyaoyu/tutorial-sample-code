83f658a3e0d69a5c0fcf5ff8b6015eced23dac54
heyaoyu <heyaoyu@youku.com>
2014-07-07 10:45:43 +0800

    Djanog tutorial part2

--- Django/mysite/polls/models.py
+++ Django/mysite/polls/models.py
@@ -3,6 +3,8 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+from django.contrib import admin
+
 
 class Poll(models.Model):
 
@@ -14,6 +16,9 @@ class Poll(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
@@ -24,3 +29,23 @@ class Choice(models.Model):
 
     def __unicode__(self):
         return self.choice_text
+
+
+#class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class PollAdmin(admin.ModelAdmin):
+
+    #fields = ['pub_date', 'question']
+    fieldsets = [
+        (None, {'fields':['question']}),
+        ('Date Information', {'fields':['pub_date'], 'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline,]
+    list_display = ['question', 'pub_date', 'was_published_recently']
+    search_fields = ['question']
+
+admin.site.register(Poll, PollAdmin)
+#admin.site.register(Choice)
